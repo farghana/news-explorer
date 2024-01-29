31ded58d4fe3110ff82653e73738b58c25c7aba2
Farghana Hussain <farghana@gmail.com>
2024-01-29 10:57:20 -0500

    Add Allan AI button to test connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react";
 
+const alanKey = '67a5079be94b4a2b3b5dcf55c5cce49e2e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand:({command, articles}) => {
+        if(command === 'newsHeadelines'){
+          console.log(articles)
+        }
+      }
+    })
+  }, []);
 	return (
 		<div className='App'>
 			<header className='App-header'>
